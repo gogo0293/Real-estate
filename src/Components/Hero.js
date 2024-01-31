abaa94b32eb2ac228b9b0ffe105b0f30834be84c
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2024-01-31 17:06:23 +0100

    added background

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -19,10 +19,52 @@ const HeroWrapper = styled.div`
     position: relative;
 `
 
-const HeroSlide = styled.div``;
-const HeroSlider = styled.div``;
-const HeroImage = styled.img``;
-const HeroContent = styled.div``;
+const HeroSlide = styled.div`
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+`;
+const HeroSlider = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const HeroImage = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+`;
+const HeroContent = styled.div`
+    position: relative;
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    max-width: 1600px;
+    width: calc(100% - 100px)
+    color: #fff;
+
+    h1{
+        font-size: clamp(1rem, 8vw, 2rem);
+        font-weight: 400;
+        text-transform: uppercase;
+        text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4)
+        text-align: left;
+        margin-bottom: 0.8rem;
+    }
+
+    p{
+        margin-bottom: 1.2rem;
+        text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4)
+    }
+`;
 
 const Hero = ({ slides }) => {
   return (
@@ -32,7 +74,7 @@ const Hero = ({ slides }) => {
                 return (
                     <HeroSlide key={index}>
                         <HeroSlider>
-                            <HeroImage />
+                            <HeroImage src={slide.image}/>
                             <HeroContent>
                                 <h1>{slide.title}</h1>
                                 <p>{slide.price}</p>
