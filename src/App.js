abdd30367fde8817eda8dcba699934318842c485
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2024-02-02 16:46:34 +0100

    added infosection

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar'
 import GlobalStyle from './GolbalStyles'
 import Hero from './Components/Hero'
 import { SliderData } from './Data/SliderData'
+import InfoSection from './Components/InfoSection'
+import { InfoData } from './Data/InfoData'
 
 const App = () => {
 
@@ -19,6 +21,7 @@ const App = () => {
       <Navbar toggle={toggle} />
       <Dorpdown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData}/> 
+      <InfoSection {...InfoData}/>
     </>
   )
 }
---
+++ src/Data/InfoData.js
@@ -0,0 +1,21 @@
+import ImageOne from '../Assets/house.jpg'
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'asd asdlasd sdf d fecre sdfsdf sdfsdf dsfsd sdf sdf sdf',
+    paragraphTwo: 'asd asdlasd sdf d fecre sdfsdf sdfsdf dsfsd sdf sdf sdf',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'asd asdlasd sdf d fecre sdfsdf sdfsdf dsfsd sdf sdf sdf',
+    paragraphTwo: 'asd asdlasd sdf d fecre sdfsdf sdfsdf dsfsd sdf sdf sdf',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: true,
+    delay: 300
+}
